5e85df71722cdbc68fdfdb40976e1d47f14cc371
pratamafarhan10 <pratamafarhan10@gmail.com>
2022-07-17 15:55:32 +0700

    create angkot ke firebase sekalian di set arahnya dari salah satau titik awal/akhir, update halte virtual apabila route_id nya berubah maka di firebase juga berubah

--- app/Http/Controllers/AngkotController.php
+++ app/Http/Controllers/AngkotController.php
@@ -71,22 +71,37 @@ class AngkotController extends Controller
                 ], 409);
             }
 
-            // also put the data into firebase
-            $angkot = $this->database->getReference('angkot/route_' . $angkot_lumen['data']['route_id'] . '/angkot_' . $angkot_lumen['data']['id'])->set([
-                'angkot_id' => $angkot_lumen['data']['id'],
-                'is_beroperasi' => false,
-                'is_full' => false,
-                'is_waiting_passengers' => false,
-                'lat' => 0,
-                'long' => 0,
-                'owner_id' => $request->input('user_id'),
-            ]);
+            try {
+                $route = Http::withToken(
+                    $request->bearerToken()
+                )->get(env('API_ENDPOINT') . 'routes/' . $angkot_lumen['data']['route_id'])->json()['data'];
 
-            $penumpang_naik_turun = $this->database->getReference('jarak_antar_angkot/angkot_' . $angkot_lumen['data']['id'])->set([
-                'angkot_id' => $angkot_lumen['data']['id'],
-                'jarak_antar_angkot_km' => 0,
-                'jarak_antar_angkot_waktu' => 0,
-            ]);
+                // also put the data into firebase
+                $angkot = $this->database->getReference('angkot/route_' . $angkot_lumen['data']['route_id'] . '/angkot_' . $angkot_lumen['data']['id'])->set([
+                    'angkot_id' => $angkot_lumen['data']['id'],
+                    'is_beroperasi' => false,
+                    'is_full' => false,
+                    'is_waiting_passengers' => false,
+                    'lat' => 0,
+                    'long' => 0,
+                    'owner_id' => $request->input('user_id'),
+                    'arah' =>  $route['titik_awal']
+                ]);
+
+                $penumpang_naik_turun = $this->database->getReference('jarak_antar_angkot/angkot_' . $angkot_lumen['data']['id'])->set([
+                    'angkot_id' => $angkot_lumen['data']['id'],
+                    'jarak_antar_angkot_km' => 0,
+                    'jarak_antar_angkot_waktu' => 0,
+                ]);
+            } catch (\Exception $e) {
+                //return error message
+                return response()->json([
+                    'status' => 'failed',
+                    'message' => $e,
+                    'data' => [],
+                ], 409);
+            }
+
 
             // check if success push data to firebase
             if ($angkot) {
@@ -152,18 +167,23 @@ class AngkotController extends Controller
                 if ($angkot_lumen['status'] != 'success') {
                     return response()->json(['error' => 'Failed to update angkot'], 400);
                 }
+                
+                $get_angkot_firebase = $this->database->getReference('angkot/route_' . $get_angkot['data']['route_id'] . '/angkot_' . $id)->getValue();
 
                 // also put the data into firebase
-                $angkot = $this->database->getReference('angkot/route_' . $angkot_lumen['data']['route_id'] . '/angkot_' . $angkot_lumen['data']['id'])->set([
-                    'angkot_id' => $angkot_lumen['data']['id'],
-                    'is_beroperasi' => $request->input('is_beroperasi'),
-                    'is_full' => $request->input('is_full'),
-                    'is_waiting_passengers' => $request->input('is_waiting_passengers'),
-                    'lat' => $request->input('lat'),
-                    'long' => $request->input('long'),
-                    'owner_id' => $request->input('owner_id'),
-                ]);
-                
+                if ($get_angkot['data']['route_id'] != $request->input('route_id')) {
+                    // push data into new route id firebase
+                    $newRoute = Http::withToken(
+                        $request->bearerToken()
+                    )->get(env('API_ENDPOINT') . 'routes/' . $request->input('route_id'))->json()['data'];
+                    $get_angkot_firebase['arah'] = $newRoute['titik_awal'];
+
+
+                    $this->database->getReference('angkot/route_' . $request->input('route_id') . '/angkot_' . $id)->set($get_angkot_firebase);
+
+                    $this->database->getReference('angkot/route_' . $get_angkot['data']['route_id'] . '/angkot_' . $get_angkot['data']['id'])->remove();
+                }
+
             } catch (\Exception $e) {
                 //return error message
                 return response()->json([
